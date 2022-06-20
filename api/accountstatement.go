301a196898e9b3d2eb26b0875f6e423c8e891f4e
prakharporwal <rsanshul99@gmail.com>
2022-06-20 16:48:10 +0530

    Account statement API completed

--- api/accountstatement.go
+++ api/accountstatement.go
@@ -4,9 +4,9 @@ import (
 	"github.com/prakharporwal/bank-server/services"
 	"net/http"
 	"strconv"
+	"time"
 
 	"github.com/gin-gonic/gin"
-	"github.com/prakharporwal/bank-server/model"
 )
 
 var klog services.Logger
@@ -14,46 +14,36 @@ var klog services.Logger
 const pageSize = 10
 
 type responseBody struct {
-	TxnId         int    `json:"txn_id"`
-	SenderId      int    `json:"sender_id"`
-	RecieverId    int    `json:"reciever_id"`
-	RecieverEmail string `json:"reciever_email"`
-	Amount        int    `json:"amount"`
+	TransactionId     int       `json:"transaction_id"`
+	AccountId         int       `json:"account_id"`
+	OtherAccount      int       `json:"other_account"`
+	OtherAccountOwner string    `json:"other_account_owner"`
+	Amount            int       `json:"amount"`
+	Type              string    `json:"type"`
+	Timestamp         time.Time `json:"timestamp"`
 }
 
-func (server *Server) GetStatement(ctx *gin.Context) {
+func (server *Server) GetAccountStatement(ctx *gin.Context) {
 	accountId, _ := strconv.Atoi(ctx.Param("account_id"))
 	pageNum, _ := strconv.Atoi(ctx.Param("page"))
 
-	transferQuery := `SELECT * FROM account_transactions_entries WHERE account_id=($1) ASC OFFSET ($2) LIMIT ($3)`
-	rows := server.store.Query(transferQuery, accountId, (pageNum-1)*pageSize, pageSize)
+	statementQuery := `SELECT T.transaction_id, T.account_id, T.other_account, A.owner_email, T.amount, T.type, T.created_at ` +
+		`FROM account_transactions_entries as T LEFT JOIN accounts as A ` +
+		`ON T.other_account=A.id WHERE T.account_id=($1) ` +
+		`ORDER BY T.created_at OFFSET ($2) LIMIT ($3);`
+	rows := server.store.Query(statementQuery, accountId, (pageNum-1)*pageSize, pageSize)
 	defer rows.Close()
 
 	var responseList []responseBody
 
 	for rows.Next() {
-		var transaction model.Transaction
-		err := rows.Scan(&transaction.Id, &transaction.FromAccountID, &transaction.ToAccountID, &transaction.Amount)
+		var response responseBody
+		err := rows.Scan(&response.TransactionId, &response.AccountId, &response.AccountId, &response.OtherAccountOwner, &response.Amount, &response.Type, &response.Timestamp)
 		if err != nil {
-			klog.Error("failed! scanning transaction : Account Id = ", accountId)
+			klog.Error("failed! scanning transaction : Account Id = ", accountId, err)
 			continue
 		}
-
-		recieverEmailQuery :=
-			`SELECT T.id, T.from_account_id, T.to_account_id, A.owner_email, T.amount` +
-				`FROM transactions as T LEFT JOIN accounts as A` +
-				` ON T.from_account_id=A.id WHERE T.from_account_id=($1) or to_account_id=$(1);`
-		rows = server.store.Query(recieverEmailQuery, accountId)
-		for rows.Next() {
-			var response responseBody
-			err := rows.Scan(response.TxnId, response.SenderId, response.RecieverId, response.RecieverEmail, response.Amount)
-			if err != nil {
-				klog.Error("Error in scanning some transactions failed at transaction, id :", transaction.Id)
-				ctx.JSON(http.StatusAccepted, responseList)
-				return
-			}
-			responseList = append(responseList, response)
-		}
+		responseList = append(responseList, response)
 	}
 
 	ctx.JSON(http.StatusOK, responseList)
